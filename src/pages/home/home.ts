ea19c8a3f15ffb99b3eabf362fb2018d069a6b55
SiljeGrimstad <silje.grimstad.95@gmail.com>
2017-04-25 13:33:54 +0200

    added tests and fixed bugs

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,7 +5,6 @@ import { GenerateReportPage } from '../generate-report/generate-report';
 import { LoginPage } from '../login/login';
 import { LogoutPage } from '../logout/logout';
 import { Observable } from 'rxjs/Observable';
-import { Chart } from 'chart.js';
 import { Http, Response, URLSearchParams, Headers } from '@angular/http';
 import { AuthService } from "../../security/auth.service";
 import { API_URL } from "../../environments/environment";
@@ -24,14 +23,19 @@ export class HomePage {
   rheumatologistDate;
   graph;
   graphList;
+  dateLimit;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, 
-  private _http: Http, private _authService: AuthService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private _http: Http, private _authService: AuthService) {
     this.flag = "../../assets/img/flag-green.png";
     this.laboratoryDate = "22.04.2017 (Suggestion)";
     this.rheumatologistDate = "20.05.2017";
     this.graph = "1"; // shows segment 1 (graph 1)
-   
+    console.log("Inside constructor");
+    var temp = new Date();
+    temp.setMonth(temp.getMonth() - 3);
+    this.dateLimit = temp;
+
   }
 
   ionViewDidLoad() {
@@ -62,45 +66,62 @@ export class HomePage {
     var disease: Array<number> = [];
 
     // iterating through all dates and adding them to the right graph
-    for (var i = 1; i < list.length - 1; i++) {
+    for (var i = 1; i < list.length; i++) {
       var element = list[i].split(',', 5); // seperating each line into a list of 5 elements
-      
-      // adding all eHAQ elements (the 5th element in the list)
-      if (((element[4] && element[0]) != null) && ((element[4].localeCompare('') != 0))) {
-        this.lineChartLabels2.push(element[0].substring(5));
-        ehaq.push(parseFloat(element[4]));
-      }
 
-      // adding all pain diary elements if the list is not empty or do not contain pain diary data
-      if (element[0] != null && (element[1] || element[2] || element[3] != null) && (element[1] || element[2] || element[3]).localeCompare('') != 0) {
+      // check valid date
+      if ((new Date(element[0])) >= this.dateLimit) {
 
-        // adding space between dates of the graph
-        if (i % 2 == 1)
+        // adding all eHAQ elements (the 5th element in the list)
+        if (((element[4] && element[0]) != null) && ((element[4].localeCompare('') != 0))) {
+          this.lineChartLabels2.push(element[0].substring(5));
+          ehaq.push(parseFloat(element[4]));
+        }
+
+        // adding all pain diary elements if the list is not empty or do not contain pain diary data
+        if (element[0] != null && (element[1] || element[2] || element[3] != null) && (element[1] || element[2] || element[3]).localeCompare('') != 0) {
           this.lineChartLabels1.push(element[0].substring(5));
-        if (i % 2 == 0)
-          this.lineChartLabels1.push("");
 
-        //  else // adding date
-        if (element[1].localeCompare('') == 0) // adding pain
-          pain.push(null);
-        else
-          pain.push(parseFloat(element[1]));
-        if (element[2].localeCompare('') == 0) // adding disease 
-          disease.push(null);
-        else
-          disease.push(parseFloat(element[2]));
+          if (element[1].localeCompare('') == 0) // adding pain
+            pain.push(null);
+          else
+            pain.push(parseFloat(element[1]));
+          if (element[2].localeCompare('') == 0) // adding disease 
+            disease.push(null);
+          else
+            disease.push(parseFloat(element[2]));
 
-        if (element[3].localeCompare('') == 0) // adding fatigue
-          fatigue.push(null);
-        else
-          fatigue.push(parseFloat(element[3]));
+          if (element[3].localeCompare('') == 0) // adding fatigue
+            fatigue.push(null);
+          else
+            fatigue.push(parseFloat(element[3]));
+        }
       }
     }
-    this.lineChartData1 = [{ data: pain, label: 'pain' },
-    { data: disease, label: 'disease activity' },
-    { data: fatigue, label: 'fatigue' }];
-    this.lineChartData2 = [{ date: ehaq, label: 'eHAQ' }];
-    console.log("Graph data loading succeeded")
+    // adding data values
+    this.lineChartData1 =
+      [{ data: pain, label: 'pain' },
+      { data: disease, label: 'disease activity' },
+      { data: fatigue, label: 'fatigue' }];
+    this.lineChartData2 = [{ data: ehaq, label: 'eHAQ' }];
+    this.fixSpacesOnGraphs();
+  }
+
+  fixSpacesOnGraphs() {
+    // haq
+    if (this.lineChartLabels2.length >= 12) {
+      for (var i = 0; i < this.lineChartLabels2.length; i++) {
+        if (i % 2 == 1)
+          this.lineChartLabels2[i] = '';
+      }
+    }
+    // Pain
+    if (this.lineChartLabels1.length >= 12) {
+      for (var i = 0; i < this.lineChartLabels1.length; i++) {
+        if (i % 2 == 1)
+          this.lineChartLabels1[i] = '';
+      }
+    }
   }
 
   // Graph info from database
@@ -110,49 +131,50 @@ export class HomePage {
       this.setGraphInfo(value.split('\n'));
     });
   }
-   // get charts from database
-    public getChart(): Observable<string> {
-        const headers: Headers = new Headers();
-        headers.append('Authorization', 'Bearer ' + this._authService.loggedInUser().authToken);
+  // get charts from database
+  public getChart(): Observable<string> {
+    const headers: Headers = new Headers();
+    headers.append('Authorization', 'Bearer ' + this._authService.loggedInUser().authToken);
 
-        var params = new URLSearchParams();
-        var object =
-            this._http.get(API_URL + '/chart/csv', {
-                headers,
-                search: params,
-            })
-                .map(res => res.text())
-                .catch(this.handleError);
-        return object;
+    var params = new URLSearchParams();
+    var object =
+      this._http.get(API_URL + '/chart/csv', {
+        headers,
+        search: params,
+      })
+        .map(res => res.text())
+        .catch(this.handleError);
+    return object;
+  }
+
+  private handleError(error: Response) {
+    // in a real world app, we may send the error to some remote logging infrastructure
+    // instead of just logging it to the console
+    if (error.status === 404) return Observable.throw('Eintrag nicht gefunden.');
+    if (error.status === 403)
+      return Observable.throw('Sie sind derzeit nicht angemeldet oder Sie haben keine Berechtigung, diese Seite aufzurufen.');
+    if (error.status === 401) return Observable.throw('Permission denied.');
+    console.error(error);
+    let errorMessage: string = null;
+    if (error.json) {
+      try {
+        errorMessage = error.json().error;
+      } catch (e) {
+        errorMessage = 'Server error: ' + (error.statusText ? error.statusText : error);
+      }
     }
+    return Observable.throw(errorMessage);
+  }
 
-    private handleError(error: Response) {
-        // in a real world app, we may send the error to some remote logging infrastructure
-        // instead of just logging it to the console
-        if (error.status === 404) return Observable.throw('Eintrag nicht gefunden.');
-        if (error.status === 403)
-            return Observable.throw('Sie sind derzeit nicht angemeldet oder Sie haben keine Berechtigung, diese Seite aufzurufen.');
-        if (error.status === 401) return Observable.throw('Permission denied.');
-        console.error(error);
-        let errorMessage: string = null;
-        if (error.json) {
-            try {
-                errorMessage = error.json().error;
-            } catch (e) {
-                errorMessage = 'Server error: ' + (error.statusText ? error.statusText : error);
-            }
-        }
-        return Observable.throw(errorMessage);
-    }
 
- 
 
- // lineChar for Pain diary graph -  need setup data for the graphs to work
+  // lineChar for Pain diary graph -  need setup data for the graphs to work
   public lineChartData1: Array<any> =
   [{ data: [0], label: 'pain' },
   { data: [0], label: 'disease activity' },
   { data: [0], label: 'fatigue' }];
   public lineChartLabels1: Array<any> = [];
+
 
   // lineChart for eHAQ graph
   public lineChartData2: Array<any> = [{ data: [0], label: 'eHAQ' }];
@@ -167,13 +189,37 @@ export class HomePage {
   };
   public maxTextLines: number = 3;
   public lineChartColors: Array<any> = [
+    { // blue
+      backgroundColor: "rgba(151,187,205,0.2)",
+      borderColor: "rgba(151,187,205,1)",
+      pointBackgroundColor: "rgba(151,187,205,1)",
+      pointBorderColor: "#fff",
+      pointHoverBackgroundColor: "#fff",
+      pointHoverBorderColor: "rgba(151,187,205,0.8)"
+    },
+    { // green
+      backgroundColor: "rgba(70,191,189,0.2)",
+      borderColor: "rgba(70,191,189,1)",
+      pointBackgroundColor: "rgba(70,191,189,1)",
+      pointBorderColor: "#fff",
+      pointHoverBackgroundColor: "#fff",
+      pointHoverBorderColor: "rgba(70,191,189,0.8)"
+    },
+    { // yellow
+      backgroundColor: "rgba(253,180,92,0.2)",
+      borderColor: "rgba(253,180,92,1)",
+      pointBackgroundColor: "rgba(253,180,92,1)",
+      pointBorderColor: "#fff",
+      pointHoverBackgroundColor: "#fff",
+      pointHoverBorderColor: "rgba(253,180,92,0.8)"
+    },
     { // grey
       backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
+      borderColor: 'rgba(148,159,177, 1)',
       pointBackgroundColor: 'rgba(148,159,177,1)',
       pointBorderColor: '#fff',
       pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+      pointHoverBorderColor: 'rgba(148,159,177,1)'
     },
     { // dark grey
       backgroundColor: 'rgba(77,83,96,0.2)',
@@ -190,6 +236,14 @@ export class HomePage {
       pointBorderColor: '#fff',
       pointHoverBackgroundColor: '#fff',
       pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    },
+    { // red
+      backgroundColor: "rgba(247,70,74,0.2)",
+      borderColor: "rgba(247,70,74,1)",
+      pointBackgroundColor: "rgba(247,70,74,1)",
+      pointBorderColor: "#fff",
+      pointHoverBackgroundColor: "#fff",
+      pointHoverBorderColor: "rgba(247,70,74,0.8)"
     }
   ];
   public lineChartLegend: boolean = true;
